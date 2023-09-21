cd37923e8b577cec250e55ad45cb65e2dd026c19
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-09-21 16:19:15 +0530

    added param

--- apps/script.js
+++ apps/script.js
@@ -1,13 +1,26 @@
+const category = [
+  {
+    name: "Computer Science",
+    value: "Computer_Science",
+  },
+  {
+    name: "biology",
+    value: "biology",
+  },
+];
 const quizForm = document.getElementById("quizForm");
-    const categorySelect = document.getElementById("option");
-    quizForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const selectedCategory = categorySelect.value;
-        if (selectedCategory === "Computer_Science") {
-            window.location.href ="computer_sciene.html";
-        } else if (selectedCategory === "geology") {
-            window.location.href = "geology.html";
-        } else {
-            alert("Please select a valid category.");
-        }
-    });
+const categorySelect = document.getElementById("option");
+
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault();
+  var selectedValue = categorySelect.value;
+  window.location.href = `computer_sciene.html?type=${selectedValue}`;
+});
+
+for (let sub of category) {
+  const option = document.createElement("option");
+  option.value = sub.value;
+  option.innerHTML = sub.name;
+
+  categorySelect.appendChild(option);
+}
